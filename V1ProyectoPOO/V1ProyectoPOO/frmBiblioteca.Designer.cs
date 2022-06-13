b578f8b9214992720532be36a603b9550adc93f4
pamelagomz <00170221@uca.edu.sv>
2022-06-13 10:47:23 -0600

    :memo:Creando Proyecto

---
+++ V1ProyectoPOO/V1ProyectoPOO/frmBiblioteca.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace V1ProyectoPOO
+{
+    partial class frmBiblioteca
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBiblioteca));
+            this.pnlPrincipal = new System.Windows.Forms.Panel();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.tlpMenu = new System.Windows.Forms.TableLayoutPanel();
+            this.btnInicio = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnEventos = new System.Windows.Forms.Button();
+            this.btnColecciones = new System.Windows.Forms.Button();
+            this.btnEjemplares = new System.Windows.Forms.Button();
+            this.btnUsuarios = new System.Windows.Forms.Button();
+            this.btnPrestamos = new System.Windows.Forms.Button();
+            this.btnReservas = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pnlPrincipal.SuspendLayout();
+            this.pnlMenu.SuspendLayout();
+            this.tlpMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlPrincipal
+            // 
+            this.pnlPrincipal.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pnlPrincipal.Controls.Add(this.pnlTop);
+            this.pnlPrincipal.Controls.Add(this.pnlMenu);
+            this.pnlPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPrincipal.Location = new System.Drawing.Point(0, 0);
+            this.pnlPrincipal.Name = "pnlPrincipal";
+            this.pnlPrincipal.Size = new System.Drawing.Size(998, 706);
+            this.pnlPrincipal.TabIndex = 0;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (5)))), ((int) (((byte) (29)))), ((int) (((byte) (64)))));
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(217, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(781, 123);
+            this.pnlTop.TabIndex = 1;
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (5)))), ((int) (((byte) (29)))), ((int) (((byte) (64)))));
+            this.pnlMenu.Controls.Add(this.tlpMenu);
+            this.pnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlMenu.Location = new System.Drawing.Point(0, 0);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(217, 706);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // tlpMenu
+            // 
+            this.tlpMenu.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.tlpMenu.ColumnCount = 1;
+            this.tlpMenu.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tlpMenu.Controls.Add(this.btnInicio, 0, 1);
+            this.tlpMenu.Controls.Add(this.btnBuscar, 0, 2);
+            this.tlpMenu.Controls.Add(this.btnEventos, 0, 3);
+            this.tlpMenu.Controls.Add(this.btnColecciones, 0, 4);
+            this.tlpMenu.Controls.Add(this.btnEjemplares, 0, 5);
+            this.tlpMenu.Controls.Add(this.btnUsuarios, 0, 6);
+            this.tlpMenu.Controls.Add(this.btnPrestamos, 0, 7);
+            this.tlpMenu.Controls.Add(this.btnReservas, 0, 8);
+            this.tlpMenu.Controls.Add(this.pictureBox1, 0, 0);
+            this.tlpMenu.Location = new System.Drawing.Point(1, 0);
+            this.tlpMenu.Name = "tlpMenu";
+            this.tlpMenu.RowCount = 9;
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 26.31579F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 9.210526F));
+            this.tlpMenu.Size = new System.Drawing.Size(216, 705);
+            this.tlpMenu.TabIndex = 0;
+            // 
+            // btnInicio
+            // 
+            this.btnInicio.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnInicio.FlatAppearance.BorderSize = 0;
+            this.btnInicio.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnInicio.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnInicio.Location = new System.Drawing.Point(3, 188);
+            this.btnInicio.Name = "btnInicio";
+            this.btnInicio.Size = new System.Drawing.Size(210, 58);
+            this.btnInicio.TabIndex = 0;
+            this.btnInicio.Text = "Inicio";
+            this.btnInicio.UseVisualStyleBackColor = true;
+            this.btnInicio.Click += new System.EventHandler(this.btnInicio_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnBuscar.FlatAppearance.BorderSize = 0;
+            this.btnBuscar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBuscar.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnBuscar.Location = new System.Drawing.Point(3, 252);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(210, 58);
+            this.btnBuscar.TabIndex = 1;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnEventos
+            // 
+            this.btnEventos.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEventos.FlatAppearance.BorderSize = 0;
+            this.btnEventos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEventos.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEventos.Location = new System.Drawing.Point(3, 316);
+            this.btnEventos.Name = "btnEventos";
+            this.btnEventos.Size = new System.Drawing.Size(210, 58);
+            this.btnEventos.TabIndex = 2;
+            this.btnEventos.Text = "Eventos";
+            this.btnEventos.UseVisualStyleBackColor = true;
+            this.btnEventos.Click += new System.EventHandler(this.btnEventos_Click);
+            // 
+            // btnColecciones
+            // 
+            this.btnColecciones.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnColecciones.FlatAppearance.BorderSize = 0;
+            this.btnColecciones.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnColecciones.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnColecciones.Location = new System.Drawing.Point(3, 380);
+            this.btnColecciones.Name = "btnColecciones";
+            this.btnColecciones.Size = new System.Drawing.Size(210, 58);
+            this.btnColecciones.TabIndex = 3;
+            this.btnColecciones.Text = "Colecciones";
+            this.btnColecciones.UseVisualStyleBackColor = true;
+            this.btnColecciones.Click += new System.EventHandler(this.btnColecciones_Click);
+            // 
+            // btnEjemplares
+            // 
+            this.btnEjemplares.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnEjemplares.FlatAppearance.BorderSize = 0;
+            this.btnEjemplares.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEjemplares.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnEjemplares.Location = new System.Drawing.Point(3, 444);
+            this.btnEjemplares.Name = "btnEjemplares";
+            this.btnEjemplares.Size = new System.Drawing.Size(210, 58);
+            this.btnEjemplares.TabIndex = 4;
+            this.btnEjemplares.Text = "Ejemplares";
+            this.btnEjemplares.UseVisualStyleBackColor = true;
+            this.btnEjemplares.Click += new System.EventHandler(this.btnEjemplares_Click);
+            // 
+            // btnUsuarios
+            // 
+            this.btnUsuarios.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnUsuarios.FlatAppearance.BorderSize = 0;
+            this.btnUsuarios.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUsuarios.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnUsuarios.Location = new System.Drawing.Point(3, 508);
+            this.btnUsuarios.Name = "btnUsuarios";
+            this.btnUsuarios.Size = new System.Drawing.Size(210, 58);
+            this.btnUsuarios.TabIndex = 5;
+            this.btnUsuarios.Text = "Usuarios";
+            this.btnUsuarios.UseVisualStyleBackColor = true;
+            this.btnUsuarios.Click += new System.EventHandler(this.btnUsuarios_Click);
+            // 
+            // btnPrestamos
+            // 
+            this.btnPrestamos.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnPrestamos.FlatAppearance.BorderSize = 0;
+            this.btnPrestamos.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPrestamos.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnPrestamos.Location = new System.Drawing.Point(3, 572);
+            this.btnPrestamos.Name = "btnPrestamos";
+            this.btnPrestamos.Size = new System.Drawing.Size(210, 58);
+            this.btnPrestamos.TabIndex = 6;
+            this.btnPrestamos.Text = "Préstamos";
+            this.btnPrestamos.UseVisualStyleBackColor = true;
+            this.btnPrestamos.Click += new System.EventHandler(this.btnPrestamos_Click);
+            // 
+            // btnReservas
+            // 
+            this.btnReservas.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReservas.AutoSize = true;
+            this.btnReservas.FlatAppearance.BorderSize = 0;
+            this.btnReservas.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReservas.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnReservas.Location = new System.Drawing.Point(3, 636);
+            this.btnReservas.Name = "btnReservas";
+            this.btnReservas.Size = new System.Drawing.Size(210, 66);
+            this.btnReservas.TabIndex = 7;
+            this.btnReservas.Text = "Reservas";
+            this.btnReservas.UseVisualStyleBackColor = true;
+            this.btnReservas.Click += new System.EventHandler(this.btnReservas_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image) (resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(207, 179);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmBiblioteca
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 706);
+            this.Controls.Add(this.pnlPrincipal);
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.Name = "frmBiblioteca";
+            this.Text = "Biblioteca";
+            this.pnlPrincipal.ResumeLayout(false);
+            this.pnlMenu.ResumeLayout(false);
+            this.tlpMenu.ResumeLayout(false);
+            this.tlpMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize) (this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnEventos;
+        private System.Windows.Forms.Button btnColecciones;
+        private System.Windows.Forms.Button btnEjemplares;
+        private System.Windows.Forms.Button btnUsuarios;
+        private System.Windows.Forms.Button btnPrestamos;
+        private System.Windows.Forms.Button btnReservas;
+
+        private System.Windows.Forms.Button btnInicio;
+
+        private System.Windows.Forms.Panel pnlPrincipal;
+
+        private System.Windows.Forms.TableLayoutPanel tlpMenu;
+
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.Panel pnlMenu;
+
+        private System.Windows.Forms.Panel pnlTop;
+
+        #endregion
+    }
+}
---
+++ V1ProyectoPOO/V1ProyectoPOO/frmBiblioteca.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace V1ProyectoPOO
+{
+    public partial class frmBiblioteca : Form
+    { 
+        private Button currentButton;
+        private int tempIndex;
+        
+        public frmBiblioteca()
+        {
+            InitializeComponent();
+        }
+        
+        private void ActivateButton(Object btnSender)
+        {
+            if (btnSender != null)
+            {
+                if (currentButton != (Button) btnSender)
+                {
+                    DisableButton();
+                    currentButton = (Button) btnSender;
+                    currentButton.BackColor = Color.Bisque;
+                    currentButton.ForeColor = Color.Black;
+                }
+            }
+        }
+
+        private void DisableButton()
+        {
+            foreach (Control previousBtn in tlpMenu.Controls)
+            {
+                if (previousBtn.GetType() == typeof(Button))
+                {
+                    previousBtn.BackColor = Color.FromArgb(5, 29,64);
+                    previousBtn.ForeColor = Color.White;
+                }
+            }
+        }
+
+
+        private void btnInicio_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnEventos_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnColecciones_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnEjemplares_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnUsuarios_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnPrestamos_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+
+        private void btnReservas_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender);
+        }
+    }
+}
